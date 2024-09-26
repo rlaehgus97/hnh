f291e045161f41d8c1c462dfb3b833d086f477f6
rlaehgus97 <starkim625@gmail.com>
2024-09-26 09:43:45 +0900

    random added

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import random as r
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"msg": "hello"}
+
+@app.predict("/")
+async def predict():
+    hotdog = r.randint(1)
+    if hotdog == 0:
+        return {"It is hotdog"}
+    else:
+        return {"It is not hotdog"}
